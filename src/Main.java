3388ce661cd60e62a46e2c4d65d31ef1005821e1
bloblyblo <steven.armand.lee@gmail.com>
2016-04-26 14:27:01 +0800

    add keyboard function and added proper output and input

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ import java.util.Random;
 public class Main {
 	final static char[] CHARACTERS= {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
 		'0','1','2','3','4','5','6','7','8','9','!','@','#','$','%','^','&','*','(',')','{','}','[',']',';'};
+	final static int MAX_LENGTH_OF_INPUT = 100;
+	final static int MAX_CHARACTER_LENGTH = 8;
 	
 	public static String SHA1(String input) throws NoSuchAlgorithmException
 	{
@@ -15,8 +17,6 @@ public class Main {
 		{
 			stringBuffer.append(Integer.toString((result[i] & 0xff)+0x100,16).substring(1));
 			
-			
-			
 		}
 		return stringBuffer.toString();
 	}
@@ -25,21 +25,42 @@ public class Main {
 	public static String SSHA1(String input) throws NoSuchAlgorithmException
 	{
 		String hash = SHA1(input);
-		return hash.substring(0, 4);
+		return hash.substring(0, MAX_CHARACTER_LENGTH);
 	}
 	
+	public static boolean isBothStringEqual(String stringOne, String stringTwo)
+	{
+		return stringOne.equals(stringTwo);
+	}
+	
+	public static void printResultInfo(String input)
+	{
+		System.out.println();
+		System.out.println("The first input is " + input);
+		try {
+			System.out.println("The hash of the input using SHA1 function is " + Main.SHA1(input));
+			System.out.println("The hash of the input using SSHA1 function is " + Main.SSHA1(input));
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
 	
 	public static void findCollusion(String input) throws NoSuchAlgorithmException
-	{
-		int counter = 0+(int)'A';
+	{	
 		Random r = new Random();
 		StringBuffer randomWord = new StringBuffer();
 		randomWord.append(input);
 		
+		//to add the first random character so that the first comparision isn't the same input
 		randomWord.append(Main.CHARACTERS[r.nextInt(Main.CHARACTERS.length)]);
-		while(!Main.SSHA1(input).equals(Main.SSHA1(randomWord.toString())))
+		
+		while(!isBothStringEqual(Main.SSHA1(input),Main.SSHA1(randomWord.toString())))
 		{
-			if(randomWord.length()>10)
+			if(randomWord.length()>MAX_LENGTH_OF_INPUT)
 			{
 				randomWord.delete(0, randomWord.length());
 				randomWord.append(input);
@@ -47,16 +68,15 @@ public class Main {
 			randomWord.append(Main.CHARACTERS[r.nextInt(Main.CHARACTERS.length)]);
 			
 		}
+		Main.printResultInfo(input);
+		Main.printResultInfo(randomWord.toString());
 	
-		
-		
-		System.out.println(randomWord.toString());
 	}
 
 	public static void main(String[] args) throws NoSuchAlgorithmException {
 		// TODO Auto-generated method stub
 		
-		Main.findCollusion("sl820");
+		Main.findCollusion(Keyboard.readString("Please input the string to find the collusion: "));
 		
 	}
 
